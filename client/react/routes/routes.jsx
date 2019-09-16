27d41d098aecaf3ccce765d414f77026b0d5cae7
Main <hohohovip1@gmail.com>
2019-09-16 21:34:36 +0700

    basic complete

--- client/react/routes/routes.jsx
+++ client/react/routes/routes.jsx
@@ -1,52 +1,80 @@
 import React from "react";
 const NodeRSA = require('node-rsa');
 const key = new NodeRSA({ b: 512 });
-const keyPair = key.generateKeyPair();
 var SHA256 = require("crypto-js/sha256");
 
 export class MainRoute extends React.Component{
     constructor(props){
         super(props);
-        this.state={
+        this.initData = {
             text:"",
             encryptedText:"",
             otherEnText:"",
-            result:""
+            result:"",
+            hash:"",
+            otherHash:""
         };
+        this.state = {
+            ...this.initData
+        };
+        this.getKeyPair();
     };
+
+getKeyPair = () => {
+     this.keyPair = key.generateKeyPair();
+}
 
 handleEncrypt = () => {
     let {text} = this.state;
-    const encryptedText = keyPair.encrypt(text, 'base64');
-    this.setState({encryptedText: encryptedText,otherEnText: encryptedText});
+    const encryptedText = this.keyPair.encrypt(text, 'base64');
+    this.setState({encryptedText: encryptedText, otherEnText: encryptedText});
 }
 
 handleDecrypt = () => {
     let {otherEnText} = this.state;
-    const decrypted = keyPair.decrypt(otherEnText, 'utf8');
+    const decrypted = this.keyPair.decrypt(otherEnText, 'utf8');
     this.setState({result: decrypted});
 }
 
-handleHash = () => {
+handleIsNull = (val, ft) =>{
+    if (!val){
+        return;
+    }
+    return ft();
+}
 
+handleHash = () => {
+    let { text } = this.state;
+    let hash = SHA256(text).toString();
+    this.setState({ hash, otherHash: hash })
 }
     render(){
         console.log(process.env.STATIC_DIR || "build");
-        let {text, encryptedText, otherEnText, result} = this.state;
+        let {text, encryptedText, otherEnText, result, hash} = this.state;
 
         return(
             <div id="main-route">
-                <label htmlFor="text">Nhập text:</label>
-                <input type="text" id="text" name="text" value={text} onChange={e => this.setState({text: e.target.value})} />
-                <button onClick={this.handleEncrypt}>Encrypt</button>
-                <div className="textBox">{encryptedText}</div>
+                <div style={{ marginBottom: "30px" }}>
+                    <button onClick={() => this.setState({ ...this.initData })}>Reset</button>
+                    <button style={{ marginLeft: "30px" }} onClick={() => {
+                        this.getKeyPair()
+                    }}>Generate key pair</button>
+                </div>
+                <div className="decrypting">
+                    <label htmlFor="text">Nhập text:</label>
+                    <input type="text" id="text" name="text" value={text} onChange={e => this.setState({text: e.target.value, hash:"", encryptedText:""})} />
+                    <button onClick={() => this.handleIsNull(text,this.handleEncrypt)}>Encrypt</button>
+                    <button onClick={() => this.handleIsNull(text,this.handleHash)}>Hash</button>
+                    <div className="textBox">{encryptedText}</div>
+                    <div className="textBox">{hash}</div>
+                </div>
                 <label htmlFor="decryptText">Encrypted text</label>
                 <input type="text" name="decryptText" value={otherEnText} onChange={e => this.setState({otherEnText: e.target.value})}/>
-                <button onClick={this.handleDecrypt}>Decrypt</button>
+                <button onClick={() => this.handleIsNull(otherEnText,this.handleDecrypt)}>Decrypt</button>
                 <div className="result">Kết quả giải mã: {result}</div>
-                <br>
+                <br/>
                 <label htmlFor="hash"></label>
-                <button onClick="this.handleHash">Hash</button>
+                
             </div>
         );
     }
